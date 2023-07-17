33f1e4244ce40cd7703381fb83f8c3381716cbd6
Mert Dede <85309911+Mertd19@users.noreply.github.com>
2023-07-17 22:51:32 +0300

    Add files via upload

---
+++ MergeSort/MergeSort.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MergeSort
+{
+    public class MergeSort
+    {
+        static void Main(string[] args)
+        {
+            var arr = new int[] { 16, 40, 44, 12, 55, 19, 6 };
+            foreach (var item in arr)
+            {
+                Console.Write($"{item,-5}");
+            }
+            Console.WriteLine();
+
+            //Merge Sort
+            Console.WriteLine("\nMerge Sort Küçük -> Büyük ");
+            DataStructures.SortingAlgorithms.MergeSort.Sort<int>(arr);
+            foreach (var item in arr)
+            {
+                Console.Write($"{item,-5}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Queue/Queue.cs
@@ -0,0 +1,50 @@
+﻿using DataStructures.Queue;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue
+{
+    public class Queue
+    {
+        private int expression;
+        static void Main(string[] args)
+        {
+            var intset = new int[] { 11, 12, 13, 14, 15 };
+            var queue1 = new DataStructures.Queue.Queue<int>();
+            var queue2 = new DataStructures.Queue.Queue<int>(QueueType.LinkedList);
+
+            foreach (var c in intset)
+            {
+                Console.WriteLine(c);
+                queue1.EnQueue(c);
+                queue2.EnQueue(c);
+            }
+
+            Console.WriteLine("\nPeek");
+            Console.WriteLine($"Stack1 : {queue1.Peek()} ");
+            Console.WriteLine($"Stack2 : {queue2.Peek()} ");
+
+            Console.WriteLine("\nCount");
+            Console.WriteLine($"Stack1 : {queue1.Count} ");
+            Console.WriteLine($"Stack2 : {queue2.Count} ");
+
+            Console.WriteLine("\nDeQueue");
+            Console.WriteLine($"Stack1 : {queue1.DeQueue()} ");
+            Console.WriteLine($"Stack2 : {queue2.DeQueue()} ");
+
+            Console.WriteLine("\nCount");
+            Console.WriteLine($"Stack1 : {queue1.Count} ");
+            Console.WriteLine($"Stack2 : {queue2.Count} ");
+
+            Console.WriteLine("\nPeek");
+            Console.WriteLine($"Stack1 : {queue1.Peek()} ");
+            Console.WriteLine($"Stack2 : {queue2.Peek()} ");
+
+
+        }
+    }
+}
