b232b300bd85d6c9fe80de1fed17912e8f558c59
Mert Dede <85309911+Mertd19@users.noreply.github.com>
2023-07-17 22:50:10 +0300

    Add files via upload

---
+++ HeapSort/HeapSort.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeapSort
+{
+    public class HeapSort
+    {
+        static void Main(string[] args)
+        {
+            var arr = new int[] { 16, 40, 44, 12, 55, 19, 6 };
+            foreach (var item in arr)
+            {
+                Console.Write($"{item,-5}");
+            }
+            Console.WriteLine();
+
+            //Heap Sort
+            Console.WriteLine("\nHeap Sort Küçük -> Büyük ");
+            var heapkb = new DataStructures.Heap.MinHeap<int>(arr);
+            foreach (var item in heapkb)
+            {
+                Console.Write($"{heapkb.DeleteMinMax(),-5}");
+            }
+            Console.WriteLine();
+            Console.WriteLine("\nHeap Sort Büyük -> Küçük ");
+            var heapbk = new DataStructures.Heap.MaxHeap<int>(arr);
+            foreach (var item in heapbk)
+            {
+                Console.Write($"{heapbk.DeleteMinMax(),-5}");
+            }
+            Console.WriteLine();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ InsertionSort/InsertionSort.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsertionSort
+{
+    public class InsertionSort
+    {
+        static void Main(string[] args)
+        {
+            var arr = new int[] { 16, 40, 44, 12, 55, 19, 6 };
+            foreach (var item in arr)
+            {
+                Console.Write($"{item,-5}");
+            }
+            Console.WriteLine();
+
+            //Insertion Sort
+            Console.WriteLine("\nInsertion Sort Küçük -> Büyük ");
+            DataStructures.SortingAlgorithms.InsertionSort.Sort<int>(arr);
+            foreach (var item in arr)
+            {
+                Console.Write($"{item,-5}");
+            }
+
+            Console.WriteLine("\nInsertion Sort Büyük -> Küçük");
+            DataStructures.SortingAlgorithms.InsertionSort.Sort<int>(arr, DataStructures.Shared.SortDirection.Descending);
+            foreach (var item in arr)
+            {
+                Console.Write($"{item,-5}");
+            }
+
+            Console.WriteLine("\nInsertion Sort Küçük -> Büyük ");
+            DataStructures.SortingAlgorithms.InsertionSort.Sort<int>(arr, DataStructures.Shared.SortDirection.Asceding);
+            foreach (var item in arr)
+            {
+                Console.Write($"{item,-5}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
