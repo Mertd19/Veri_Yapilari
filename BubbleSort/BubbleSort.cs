872dd86e861a346ae0db3496f86d7f3042fbb567
Mert Dede <85309911+Mertd19@users.noreply.github.com>
2023-07-17 22:44:33 +0300

    Add files via upload

---
+++ BubbleSort/BubbleSort.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubbleSort
+{
+    public class BubbleSort
+    {
+        static void Main(string[] args)
+        {
+            var arr = new int[] { 16, 40, 44, 12, 55, 19, 6 };
+            foreach (var item in arr)
+            {
+                Console.Write($"{item,-5}");
+            }
+            Console.WriteLine();
+
+            //Bubble Sort
+            Console.WriteLine("\nBubble Sort Küçük -> Büyük ");
+            DataStructures.SortingAlgorithms.BubbleSort.Sort<int>(arr);
+            foreach (var item in arr)
+            {
+                Console.Write($"{item,-5}");
+            }
+
+            Console.WriteLine("\nBubble Sort Büyük -> Küçük");
+            DataStructures.SortingAlgorithms.BubbleSort.Sort<int>(arr, DataStructures.Shared.SortDirection.Descending);
+            foreach (var item in arr)
+            {
+                Console.Write($"{item,-5}");
+            }
+
+            Console.WriteLine("\nBubble Sort Küçük -> Büyük ");
+            DataStructures.SortingAlgorithms.BubbleSort.Sort<int>(arr, DataStructures.Shared.SortDirection.Asceding);
+            foreach (var item in arr)
+            {
+                Console.Write($"{item,-5}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CustomTypes/Student.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace CustomTypes
+{
+    public class Student : IComparable
+    {
+        public Student()
+        {
+            
+        }
+        public Student(int ID, string Name, double GPA)
+        {
+            this.ID = ID;
+            this.Name = Name;
+            this.GPA = GPA;
+        }
+
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public double GPA { get; set; }
+
+        public int CompareTo(object? obj)
+        {
+            var other = (Student)obj;
+            //if(this.GPA < other.GPA)
+            //{
+            //    return -1;
+            //}
+            //else if (this.GPA.Equals(other.GPA))
+            //{
+            //    return 0;
+            //}
+            //else
+            //{
+            //    return 1;
+            //}
+            return this.GPA.CompareTo(other.GPA); //(Üstteki açıklama satırları ile aynı işlemi yapıyor.!) GPA 'ya göre sıralama
+            //return this.Name.CompareTo(other.Name); //İsme göre sıralama.
+            //return this.ID.CompareTo(other.ID); //ID 'ye göre sıralama.
+            
+        }
+
+        public override string ToString()
+        {
+            return $"{ID,-5}  {Name,-15}  {GPA,-5}";
+        }
+    }
+}
